b4a995c6f5b14d855db12947a429fd51d718c66b
Renato Moschetta <renatorock3@hotmail.com>
2020-08-10 22:50:25 -0300

    update react web

---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch } from "react-router-dom";
+
+import Route from "./Routes";
+
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
+import ForgotPassowrd from "../pages/ForgotPassword";
+import ResetPassword from "../pages/ResetPassword";
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassowrd} />
+    <Route path="/reset-password" component={ResetPassword} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+  </Switch>
+);
+
+export default Routes;

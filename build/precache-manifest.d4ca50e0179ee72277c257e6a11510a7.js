99c2d58644f298c39054e742cdc398f468352d5b
Renato Moschetta <renatorock3@hotmail.com>
2021-05-06 23:25:16 -0300

    updated

---
+++ build/precache-manifest.d4ca50e0179ee72277c257e6a11510a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfd3fcc601a7742e02eac283b935e7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c654c992db29fd53fc3",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "0c654c992db29fd53fc3",
+    "url": "/static/js/2.07a48d6b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.07a48d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1b605db61378925af2",
+    "url": "/static/js/main.4c2d215b.chunk.js"
+  },
+  {
+    "revision": "5726b2cd1ab221ead512",
+    "url": "/static/js/runtime-main.40f7f5d3.js"
+  },
+  {
+    "revision": "a49b07ec538713cf0e3910693544c7be",
+    "url": "/static/media/logo.a49b07ec.svg"
+  },
+  {
+    "revision": "97fd5c8f8e9584511798e3dfde2529d1",
+    "url": "/static/media/placeholder.97fd5c8f.png"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
